c14fa4454f83006bf86ade24a1b110b3b0e0d631
ElenaBezro <lenyka_86@mail.ru>
2023-11-30 17:53:35 +0100

    feat: validate creating order

--- src/main/java/com/ecommerce/order/OrderDao.java
+++ src/main/java/com/ecommerce/order/OrderDao.java
@@ -1,16 +1,38 @@
 package com.ecommerce.order;
 
+import com.ecommerce.product.Product;
+import com.ecommerce.product.ProductDao;
+
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class OrderDao {
-    private List<Order> orders = new ArrayList<>();
+    private static List<Order> orders = new ArrayList<>();
 
-    public void addOrder(Order order) {
+    public void addOrder(Order order) throws Exception {
         //Validate product availability
+
+        Product product = new ProductDao().getProduct(order.getProductId());
+        int availableQuantity = -1;
+        if (product != null) {
+            availableQuantity = product.getStockQuantity();
+        }
+        if (availableQuantity == -1) {
+            //TODO: use custom Exception
+            throw new Exception("Wrong product id");
+        }
+        else if (availableQuantity < order.getQuantity()) {
+            //TODO: use custom Exception
+            throw new Exception("Not sufficient product quantity");
+        }
         orders.add(order);
         System.out.println("add order");
+    }
+
+    public List<Order> getAllOrders() {
+        System.out.println("getAllOrders");
+        return orders;
     }
 
 }
--- src/main/java/com/ecommerce/order/OrderResource.java
+++ src/main/java/com/ecommerce/order/OrderResource.java
@@ -1,17 +1,34 @@
 package com.ecommerce.order;
 
 
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+import java.util.List;
 
 @Path("/order")
 public class OrderResource {
-    private OrderDao orderDao = new OrderDao();
+    private static  OrderDao orderDao = new OrderDao();
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
-    public void addOrder(Order order) {
-        orderDao.addOrder(order);
+    public Response addOrder(Order order) {
+        try {
+            orderDao.addOrder(order);
+            return Response.status(Response.Status.CREATED)
+                    .build();
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity(e.getMessage())
+                    .build();
+        }
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Order> getAllOrders() {
+        return orderDao.getAllOrders();
     }
 }
